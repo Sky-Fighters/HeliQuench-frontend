7c6aa6e42af4878eef58bfee652490af35d5b035
Emerolde <Emerolde1234@gmail.com>
2023-08-08 18:45:38 +0000

    start

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+kaboom();
+
+
+scene("start", () => {
+    loadFont()
+    const bgColor = color(122, 48, 108); 
+
+    add([
+        rect(width(), height()), 
+        bgColor,
+        pos(width() / 2, height() / 2),
+        anchor("center"),
+    ]);
+
+    const startGame = add([
+        text("Press Enter to Continue", {
+          transform: (idx, ch) => ({
+            color: rgb(255, 255, 255),
+            pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
+            scale: wave(1, 1.2, time() * 3 + idx),
+            angle: wave(-24, 9, time() * 3 + idx),
+          }),
+        }),
+        pos(width() / 2, height() / 1.5),
+        scale(0.75, 0.75),
+        anchor("center"),
+        area(),
+      ]);
+    
+    const titleText = add([
+        text("HeliQuench", {
+          transform: (idx, ch) => ({
+            color: rgb(255, 255, 255),
+            pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
+            scale: wave(1, 1.2, time() * 3 + idx),
+            angle: wave(-24, 9, time() * 3 + idx),
+          }),
+        }),
+        pos(width()/2,startGame.pos.y/2),
+        scale(1.5),
+        anchor("center"),
+        area(),
+      ])
+    // onKeyPress("enter", () => {
+    //     go("game");   
+    //   });
+});
+
+
+go("start");
+
+// start
+
 kaboom(); // adds the checkerboard background to start all the kaboom assets 
 
 loadSprite("background", "./images/forest.jpg");
