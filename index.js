944a4f8862d0ea8c55502fbd94ac042567f65978
engineerErica <ericaponce2002@gmail.com>
2023-08-08 18:45:53 +0000

    created game scene to hold the code

--- index.js
+++ index.js
@@ -7,126 +7,138 @@ loadSprite("tree", "./images/tree.png");
 loadSprite("burning-tree", "./images/burning-tree.png");
 
 
+scene("game", () => {
+
+  // Define player movement speed (pixels per second)
+  // GAME VARIABLES 
+  const SPEED = 320;
+  let canSpawnWaterBalloon = true;
+
+  // GAME OBJECTS 
 
 
-// Define player movement speed (pixels per second)
-// GAME VARIABLES 
-const SPEED = 320;
-let canSpawnWaterBalloon = true;
-
-// GAME OBJECTS 
-
-
-const background = add([
+  const background = add([
     sprite("background"),
     scale(1.6),
-    
-    
-]);
-
-const player = add([
-	sprite("helicopter"),   // sprite() component makes it render as a sprite
-	pos(160, 140),     // pos() component gives it position, also enables movement
-	rotate(0),        // rotate() component gives it rotation
-	anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
-	scale(0.5), 
-]);
 
 
+  ]);
+
+  const player = add([
+    sprite("helicopter"), // sprite() component makes it render as a sprite
+    pos(160, 140), // pos() component gives it position, also enables movement
+    rotate(0), // rotate() component gives it rotation
+    anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
+    scale(0.5),
+  ]);
+
+  
+  // let score = 0;
+  // const scoreLabel = add([
+  //   text(score),
+  //   pos(24, 24),
+  // ]);
+  
+  // onUpdate(() => {
+  //   score++;
+  //   scoreLabel.text = score;
+  // });
+
+  onKeyDown("a", () => {
+    // .move() is provided by pos() component, move by pixels per second
+    player.move(-SPEED, 0)
+  })
+
+  onKeyDown("d", () => {
+    player.move(SPEED, 0)
+  })
+
+  onKeyDown("w", () => {
+    player.move(0, -SPEED)
+  })
+
+  onKeyDown("s", () => {
+    player.move(0, SPEED)
+  })
 
 
-onKeyDown("a", () => {
-	// .move() is provided by pos() component, move by pixels per second
-	player.move(-SPEED, 0)
-})
 
-onKeyDown("d", () => {
-	player.move(SPEED, 0)
-})
+  onKeyDown("space", () => {
+    const score = 0;
+    if (canSpawnWaterBalloon) {
+      canSpawnWaterBalloon = false; // Disable spawning while a water balloon is active
 
-onKeyDown("w", () => {
-	player.move(0, -SPEED)
-})
+      const waterBalloon = add([
+        sprite("water"),
+        pos(player.pos.x, player.pos.y),
+        scale(0.5),
+        move(DOWN, 250),
+        area(),
+      ]);
 
-onKeyDown("s", () => {
-	player.move(0, SPEED)
-})
+      // After a delay, enable spawning again
+      wait(1, () => {
+        canSpawnWaterBalloon = true;
+      })
 
+      waterBalloon.onCollide("fire", (fire) => {
+        destroy(fire);
+        destroy(waterBalloon);
 
+        spawnNewObject(fire.pos); // Create a new object in place of the water balloon
+      })
 
-onKeyDown("space", () => {
-  if (canSpawnWaterBalloon) {
-    canSpawnWaterBalloon = false; // Disable spawning while a water balloon is active
+    }
+  });
 
-    const waterBalloon = add([
-      sprite("water"),
-      pos(player.pos.x, player.pos.y),
-      scale(0.5),
-      move(DOWN, 250),
-      area(),
-    ]);
-
-    // After a delay, enable spawning again
-    wait(1, () => {
-      canSpawnWaterBalloon = true;
-    })
-    
-    waterBalloon.onCollide("fire", (fire) => {
-	  destroy(fire);
-	  destroy(waterBalloon);
-	  
-	  spawnNewObject(fire.pos); // Create a new object in place of the water balloon
-    })
-
-        
-  }
-});
-
-const protectTree = add([
+  const protectTree = add([
     sprite("tree"),
     scale(0.7),
     pos(100, 400),
     area(),
     "protect",
 
-]);
+  ]);
 
-function spawnTree() {
+  function spawnTree() {
     add([
-        sprite("burning-tree"),
-        scale(0.7),
-        pos(width(), height() + 10),
-        anchor("botleft"),
-        move(LEFT, 200),
-        area(),
-        "fire",
+      sprite("burning-tree"),
+      scale(0.7),
+      pos(width(), height() + 10),
+      anchor("botleft"),
+      move(LEFT, 200),
+      area(),
+      "fire",
     ]);
     wait(rand(0.5, 1.5), () => {
-        spawnTree();
+      spawnTree();
     });
-}
+  }
 
-spawnTree();
+  spawnTree();
 
-function spawnNewObject(position) {
-  const newObj = add([
-    sprite("tree"), 
-    pos(position),
-    scale(0.8),
-  ]);
-}
+  function spawnNewObject(position) {
+    const newObj = add([
+      sprite("tree"),
+      pos(position),
+      scale(0.8),
+    ]);
+  }
 
-function spawnNewFireTree(position) {
-  const newObj = add([
-    sprite("burning-tree"), 
-    pos(position),
-    scale(0.8),
-  ]);
-}
+  function spawnNewFireTree(position) {
+    const newObj = add([
+      sprite("burning-tree"),
+      pos(position),
+      scale(0.8),
+    ]);
+  }
 
-protectTree.onCollide("fire", () => {
-  destroy(protectTree);
-  spawnNewFireTree(protectTree.pos);
-  
-  
-})
+  protectTree.onCollide("fire", () => {
+    destroy(protectTree);
+    spawnNewFireTree(protectTree.pos);
+
+  })
+
+});
+
+go("game");
